94cdf2732786f643d4a79d9a50d52d0c95c04676
Enzo <91644783+aesaries@users.noreply.github.com>
2024-05-19 21:39:08 -0300

    cargando imagen de api

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const URL='https://foodish-api.com/images/pizza/';
-
-fetch(URL)
-.then(res => res.json())
-.then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-});
-
+document.addEventListener("DOMContentLoaded", () => {
+    const imgApiElement = document.getElementById("imgapi");
+  
+    fetch("https://foodish-api.com/api/")
+      .then(response => response.json())
+      .then(data => {
+        imgApiElement.src = data.image;
+      })
+      .catch(error => {
+        console.error("Error fetching the image from API:", error);
+      });
+  });
